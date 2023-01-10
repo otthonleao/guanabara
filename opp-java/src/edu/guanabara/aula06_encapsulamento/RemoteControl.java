b897cd92d03ee7052939e7807ecd8a4d1a092c25
Otthon Leão <otthonleao@hotmail.com>
2023-01-09 22:09:47 -0400

    feat(get/set): attributes and get/set at RemoteControl

--- opp-java/src/edu/guanabara/aula06_encapsulamento/RemoteControl.java
+++ opp-java/src/edu/guanabara/aula06_encapsulamento/RemoteControl.java
@@ -1,5 +1,37 @@
 package edu.guanabara.aula06_encapsulamento;
 
 public class RemoteControl {
+	//Attributes
+	private int volume;
+	private boolean turnedOn;
+	private boolean playing;
+
+	//Constructor
+	public RemoteControl() {
+		this.volume = 20;
+		this.turnedOn = false;
+		this.playing = false;
+	}
+
+	//GETTER e SETTERS
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public boolean getTurnedOn() {
+		return turnedOn;
+	}
+	public void setTurnedOn(boolean turnedOn) {
+		this.turnedOn = turnedOn;
+	}
 	
+	public boolean getPlaying() {
+		return playing;
+	}
+	public void setPlaying(boolean playing) {
+		this.playing = playing;
+	}
 }
