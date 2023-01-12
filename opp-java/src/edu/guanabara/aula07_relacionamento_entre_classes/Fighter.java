f276200791c4ca50289199c21661fafc259fc24e
Otthon Leão <otthonleao@hotmail.com>
2023-01-11 21:58:25 -0400

    feat(coding): attribute declaration in Figther class

---
+++ opp-java/src/edu/guanabara/aula07_relacionamento_entre_classes/Fighter.java
@@ -0,0 +1,11 @@
+package edu.guanabara.aula07_relacionamento_entre_classes;
+
+public class Fighter {
+	//Atributos
+	private String name;
+	private String nationality;
+	private int age;
+	private float height, weight;
+	private String category;
+	private int wins, losses, draws; //vitórias, derrotas, empates
+}
