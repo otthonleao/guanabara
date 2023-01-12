ace5afd4790ace7ff958b29ca2588fe733a99d1b
Otthon Leão <otthonleao@hotmail.com>
2023-01-11 22:37:59 -0400

    feat(code): developing publics methods ans constructor method

--- opp-java/src/edu/guanabara/aula07_relacionamento_entre_classes/Fighter.java
+++ opp-java/src/edu/guanabara/aula07_relacionamento_entre_classes/Fighter.java
@@ -7,5 +7,38 @@ public class Fighter {
 	private int age;
 	private float height, weight;
 	private String category;
-	private int wins, losses, draws; //vitórias, derrotas, empates
+	private int wins, losses, drawns; //vitórias, derrotas, empates
+
+	//Métodos Públicos
+	public void present() {
+
+	}
+
+	public void status() {
+
+	}
+
+	public void winFight() {
+
+	}
+
+	public void lossFight() {
+
+	}
+
+	public void drawnFight() {
+
+	}
+
+	//Construtor - Métodos Especiais
+	public Fighter(String name, String nationality, int age, float height, float weight, int wins, int losses, int drawns) {
+		this.name = name;
+		this.nationality = nationality;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.wins = wins;
+		this.losses = losses;
+		this.drawns = drawns;
+	}
 }
