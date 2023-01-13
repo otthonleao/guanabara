8711c07cf63ff37a97bb9fdfcd10928f4bb1f67a
Otthon Leão <otthonleao@hotmail.com>
2023-01-13 01:32:06 -0400

    feat(code): developer the Fight class using aggregation between objects to dispute

---
+++ opp-java/src/edu/guanabara/aula07_relacionamento_entre_classes/Fight.java
@@ -0,0 +1,84 @@
+package edu.guanabara.aula07_relacionamento_entre_classes;
+
+import java.util.Random;
+
+public class Fight {
+	private Fighter challenged; //desafiado
+	private Fighter challenging; //desafiante
+	private int rounds;
+	private boolean approved;
+
+	//Public Methods
+	public void schaduleFight(Fighter play1, Fighter play2) {
+		if ((play1.getCategory().equals(play2.getCategory())) && (play1 != play2)){ //Nesse caso temos que usar o '.equals' para comparação ao invés de '==' pq estamos comparando duas strings
+			this.approved = true;
+			this.challenged = play1;
+			this.challenging = play2;
+		} else {
+			this.approved = false;
+			this.challenged = null;
+			this.challenging = null;
+		}
+	}
+	public void fight() {
+		if(this.approved == true) {
+			System.out.println("##########   ULTRA EMOJI COMBAT   ##########");
+			System.out.println("##########         APROVED        ##########");
+			System.out.println("With yooouuuu... the Challenged is: ");
+			this.challenged.present();
+			System.out.println("And the Challenging is: ");
+			this.challenging.present();
+			//Sortear o resultado da luta
+			Random aleatory = new Random();
+			int winner = aleatory.nextInt(3); //Sortear 3 números: 0, 1, 2.
+			switch(winner) {
+				case 0: //Empate caso o sorteio for 0;
+					System.out.println("TIE");
+					this.challenged.drawnFight();
+					this.challenging.drawnFight();
+					break;
+				case 1: //Desafiado vence
+					System.out.println("Victory of " + this.challenged.getName());
+					this.challenged.winFight();
+					this.challenging.lossFight();
+					break;
+				case 2: //Desafiante vence
+					System.out.println("Victory of " + this.challenging.getName());
+					this.challenged.lossFight();
+					this.challenging.winFight();
+					break;
+			}
+
+		}
+	}
+
+	//Special Methods
+	public Fighter getChallenged() {
+		return challenged;
+	}
+	public void setChallenged(Fighter challenged) {
+		this.challenged = challenged;
+	}
+
+	public Fighter getChallenging() {
+		return challenging;
+	}
+	public void setChallenging(Fighter challenging) {
+		this.challenging = challenging;
+	}
+
+	public int getRounds() {
+		return this.rounds;
+	}
+	public void setRounds(int rounds) {
+		this.rounds = rounds;
+	}
+
+	public boolean approved() {
+		return this.approved;
+	}
+	public void setApproved(boolean approved) {
+		this.approved = approved;
+	}
+
+}
