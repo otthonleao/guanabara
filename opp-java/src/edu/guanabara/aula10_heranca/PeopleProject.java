f8a9be67f1b0da8af6e9f05956ac09ec4d4280cf
Otthon Leão <otthonleao@hotmail.com>
2023-01-13 04:19:31 -0400

    style(main): add new arguments

--- opp-java/src/edu/guanabara/aula10_heranca/PeopleProject.java
+++ opp-java/src/edu/guanabara/aula10_heranca/PeopleProject.java
@@ -2,6 +2,29 @@ package edu.guanabara.aula10_heranca;
 
 public class PeopleProject {
 	public static void main(String[] args) {
-		
+		Person person = new Person();
+		Student personStudent = new Student();
+		Professor personProfessor = new Professor();
+		Employee personEmployee = new Employee();
+
+		person.setName("Otthon");
+		personStudent.setName("Roany");
+		personProfessor.setName("Guanabara");
+		personEmployee.setName("Ronie");
+
+		System.out.println(person.toString());
+		System.out.println(personStudent.toString());
+		System.out.println(personProfessor.toString());
+		System.out.println(personEmployee.toString());
+
+		person.setSex('M');
+		personStudent.setSex('W');
+		personProfessor.setSex('M');
+		personEmployee.setSex('M');
+
+		System.out.println("\n" + person.toString());
+		System.out.println(personStudent.toString());
+		System.out.println(personProfessor.toString());
+		System.out.println(personEmployee.toString());
 	}
 }
